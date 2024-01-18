9a6dce55a8e9d1e76a14480a3c27978d1f6ba201
Aman Kumar <138701271+Amankr2099@users.noreply.github.com>
2024-01-18 23:04:33 +0530

    Update function.js

--- function.js
+++ function.js
@@ -4,17 +4,40 @@ const spellUse = $(".spell-use")
 const url = "https://harry-potter-api-en.onrender.com/spells/"
 const button = $(".button-section button")
 
+
+const handleClick = async () => {
+    randomId = Math.floor(Math.random() * 72) + 1;
+    try {
+        const response = await fetch(url + randomId)
+        const data = await response.json()
+        spellName.fadeIn("slow", () => {
+            spellName.text(data.spell)
+        })
+        spellUse.fadeIn("slow", () => {
+            spellUse.text(`"${data.use}"`)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
 button.click(() => {
     button.text("New Spell")
-    randomId = Math.floor(Math.random() * 72) + 1;
-    fetch(url + randomId)
-        .then((response) => response.json())
-        .then((data) => {
-            spellName.fadeIn("slow", () => {
-                spellName.text(data.spell)
-            })
-            spellUse.fadeIn("slow", () => {
-                spellUse.text(`"${data.use}"`)
-            })
-        })
+    handleClick()
 })
+
+// button.click(() => {
+//     button.text("New Spell")
+//     randomId = Math.floor(Math.random() * 72) + 1;
+//     fetch(url + randomId)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             spellName.fadeIn("slow", () => {
+//                 spellName.text(data.spell)
+//             })
+//             spellUse.fadeIn("slow", () => {
+//                 spellUse.text(`"${data.use}"`)
+//             })
+//         })
+// })
